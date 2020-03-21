a45ba05c80a42d4b6e772deb05186496aa3c2318
Vignesh Suresh Kumar <vigneshsureshkumar17@gmail.com>
2020-03-20 23:51:39 -0400

    This is the first commit.

---
+++ app/src/main/java/com/example/quizbowlquestions2/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.quizbowlquestions2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.quizbowlquestions2.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
